91b62c9e59210c9e71f361d093cf9c0f653dfb0c
Letícia Leal <leticialeal@ciandt.com>
2022-03-30 11:47:55 -0300

    criaçao dos filtros e ordenadores

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -2,9 +2,13 @@ import styles from './Cardapio.module.scss';
 import {ReactComponent as Logo} from "assets/logo.svg";
 import Buscador from './Buscador';
 import { useState } from 'react';
+import Filtros from './Filtros';
+import Ordeador from './Ordenador';
 
 export default function Cardapio(){
     const [busca, setBusca] = useState("");
+    const [filtro, setFiltro] = useState<number | null>(null);
+    const [ordenador, setOrdenador] = useState("");
 
     return (
         <main>
@@ -20,6 +24,10 @@ export default function Cardapio(){
                 busca={busca} 
                 setBusca={setBusca}
                 />
+                <div className={styles.cardapio__titulo}>
+                    <Filtros filtro={filtro} setFiltro={setFiltro}/>
+                    <Ordeador ordenador={ordenador} setOrdenador={setOrdenador}/>
+                </div>
             </section>
         </main>
     )
